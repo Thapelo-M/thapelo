79d94197cc2d1794ff5eb0730cebae5c42598fd3
Thapelo-M <keomothokoa@gmail.com>
2024-07-02 15:19:27 +0200

    Rectifying issues on public path commt

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/{thapelo-m.github.io}/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/{thapelo}/' : '/',
   transpileDependencies: true
 })
